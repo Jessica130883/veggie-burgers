ef8a1ef45843485bb5d6390a98b94f6bb45c0905
Jessica Abdoulahi <abdoulahi.jessica@gmail.com>
2024-05-16 11:13:47 +0200

    Code de connexion complet avec l'utilisation de la classe Databse

--- admin/database.php
+++ admin/database.php
@@ -1,10 +1,25 @@
-<?php 
-try{
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=veggie_burger;charset=utf8', 'admin_veggie', 'veggiemdp');
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?php
+ 
+ class Database {
+    private $bdd;  
 
-} catch (PDOException $e) {
-die('Erreur de connexion : ' . $e->getMessage());
+    public function __construct() {  
+        try {
+            $this->bdd = new PDO('mysql:host=127.0.0.1;dbname=veggie_burger;charset=utf8', 'admin_veggie', 'veggiemdp');
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Erreur de connexion : ' . $e->getMessage());
+        }
+    }
+
+    
+    public function getBdd() {
+        return $this->bdd;  
+    }
 }
+
+
+
+
 
 ?>
