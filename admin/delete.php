3ec6aae6c032469ce4f994c8bf3e1dda11f5f42c
Jessica Abdoulahi <abdoulahi.jessica@gmail.com>
2024-05-22 01:59:12 +0200

    Mise en forme du formulaire delete.php

---
+++ admin/delete.php
@@ -0,0 +1,56 @@
+<?php
+require 'database.php';
+
+
+function checkInput($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="Stylesheet" href="../Stylesheet.css">
+    <title>Modifier un article</title>
+</head>
+
+<body>
+<h1 class="text-logo"><span class="bi-shop"></span> Veggie Burger <span class="bi-shop"></span></h1>
+<div class="container admin">
+    <div class="row">
+        <div class="col-sm-6">
+            <h1><strong>Supprimer un item</strong></h1>
+            <br>
+            <form class="form" action="delete.php" role="form" method="post">
+                <input type="hidden" name="id" value="<?php echo $id; ?>"/> 
+               <p class="alert alert-warning">Etes-vous sûr de vouloir supprimer ?</p>
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-warning"> Oui </button>
+                   
+                    <a class="btn btn-default" href="index.php"> Non </a>
+                </div>
+            </form>
+        </div> 
+       
+            
+        </div>   
+    </div>
+</div>   
+</body>
+</html>
